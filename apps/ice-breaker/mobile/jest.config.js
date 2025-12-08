e0543d32193c3b9cff07d30f39b75ed679550bc8
kgridou <32600911+kgridou@users.noreply.github.com>
2025-12-08 12:50:21 +0100

    wip

---
+++ apps/ice-breaker/mobile/jest.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  preset: '../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        stringifyContentPathRegex: '\\.(html|svg)$',
+        astTransformers: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+
+  coverageDirectory:
+    '../../../coverage/apps/ice-breaker/mobile',
+
+  displayName: 'nativescript-safety',
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
